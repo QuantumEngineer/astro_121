f3d6f7bf377b43fa34dd78f7c21731d5e49b0074
QuantumEngineer <kahn@berkeley.edu>
2014-02-26 18:08:13 -0800

    Updates

--- lab_2/data/2_1_1/mixercontrol.py
+++ lab_2/data/2_1_1/mixercontrol.py
@@ -2,20 +2,20 @@ import DFEC
 import numpy as np 
 import pylab as py 
 
-vlo = 1e6 #10 KHz
+vlo = 1e6 #1 KHz
 delv = .05*vlo
 vsighigh = vlo + delv
 vsiglow = vlo - delv
-vsample = {str(vsiglow):(2.0*vsiglow),str(vsighigh):(2.0*vsighigh)} 
+vsample = 10e6 
 N = 2**14
 
 for vsig in [vsiglow, vsighigh]: 
-   # DFEC.set_srs(1, freq=vsig, dbm=0, pha=180)
-   # DFEC.set_srs(2, freq=vlo, dbm=0, pha=180)
+    DFEC.set_srs(2, freq=vsig,dbm=1, pha=0)
+    DFEC.set_srs(1, freq=vlo, dbm=7, pha=0)
     print 'RF srs_2 = ' + str(vsig/1000000.0) + ' MHz  and V = .75'  
     print 'LO srs_1 = ' + str(vlo/1000000.0) + ' MHz and V = .25' 
     raw_input("Start data collection?")
-    data = DFEC.sampler(N,vsample[str(vsig)])
+    data = DFEC.sampler(N,vsample)
     filename = "vsig_%.1f.npz" % vsig
     np.savez(filename,data)
     print "Saved %s" % filename
--- lab_2/data/2_1_1/powerspectrum.py
+++ lab_2/data/2_1_1/powerspectrum.py
@@ -3,7 +3,7 @@ import pylab as py
 import radiolab as rd
 
 N = 2**14
-vsample = 1.0e6
+vsample = 10e6
 delt = 1.0/vsample
 time_array = np.arange(0,N)*delt
 filename= "vsig_1050000.0.npz" 
@@ -13,9 +13,10 @@ data = np.load(filename)["arr_0"]
 fft_data = abs(np.fft.fft(data))**2 
 print np.fft.fftfreq(N,delt)
 py.subplot(2,1,1)
-py.plot(time_array[0:2**12],data[0:2**12])
+py.plot(time_array[0:2**9]*10e6,data[0:2**9])
 
 py.subplot(2,1,2) 
-py.plot(np.fft.fftfreq(N,delt),fft_data)
+py.plot(np.fft.fftfreq(N,delt)/1e6,fft_data)
+
 
 py.show()
