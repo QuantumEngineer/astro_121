52e5af2cbaf450fb6a8679ac11fc49298d78d3bd
QuantumEngineer <kahn@berkeley.edu>
2014-02-27 17:43:05 -0800

    Updates

--- lab_2/data/2_1_1/mixercontrol.py
+++ lab_2/data/2_1_1/mixercontrol.py
@@ -2,21 +2,21 @@ import DFEC
 import numpy as np 
 import pylab as py 
 
-vlo = 1e6 #1 KHz
+vlo = 2e6 
 delv = .05*vlo
 vsighigh = vlo + delv
 vsiglow = vlo - delv
-vsample = 10e6 
+vsample = 20e6 
 N = 2**14
 
 for vsig in [vsiglow, vsighigh]: 
-    DFEC.set_srs(2, freq=vsig,dbm=1, pha=0)
-    DFEC.set_srs(1, freq=vlo, dbm=7, pha=0)
-    print 'RF srs_2 = ' + str(vsig/1000000.0) + ' MHz  and V = .75'  
-    print 'LO srs_1 = ' + str(vlo/1000000.0) + ' MHz and V = .25' 
+   # DFEC.set_srs(2, freq=vsig,dbm=1, pha=0)
+   # DFEC.set_srs(1, freq=vlo, dbm=7, pha=0)
+    print 'RF srs_2 = ' + str(vsig/1000000.0) + ' MHz  and dBm = 0'  
+    print 'LO srs_1 = ' + str(vlo/1000000.0) + ' MHz and dBM = 0' 
     raw_input("Start data collection?")
     data = DFEC.sampler(N,vsample)
     filename = "vsig_%.1f.npz" % vsig
-    np.savez(filename,data)
+    np.savez(filename,data=data,vsample=vsample,vsig=vsig)
     print "Saved %s" % filename
 
--- lab_2/data/2_1_1/powerspectrum.py
+++ lab_2/data/2_1_1/powerspectrum.py
@@ -3,41 +3,40 @@ import pylab as py
 
 
 N = 2**14
-vsample = 10e6
+vsample = 20e6
 delt = 1.0/vsample
 time_array = np.arange(0,N)*delt
-filename= "vsig_950000.0.npz" 
-data = np.load(filename)["arr_0"]
+filename= "vsig_1900000.0.npz" 
+data = np.load(filename)["data"]
 fft_data = abs(np.fft.fft(data))**2 
 
 
 py.subplot(2,1,1)
-py.plot(time_array[0:2**9]*1e6,data[0:2**9])
+py.plot(time_array[0:2**10]*1e6,data[0:2**10])
 py.xlim([10,40])
 py.xlabel('time ($\mu s$)')
 py.ylabel('Voltage (V)')
-py.title('Lower Side Bound')
 
 py.subplot(2,1,2) 
+
 py.plot(np.fft.fftfreq(N,delt)/1e6,fft_data/1e6)
-py.xlim([-3,3])
+
 py.ylabel('Intensity (dBm)')
 py.xlabel('Frequency (MHz)')
 
 
 py.figure()
 
-filename= "vsig_1050000.0.npz" 
-data = np.load(filename)["arr_0"]
-fft_data = abs(np.fft.fft(data))**2 
+filename= "vsig_2100000.0.npz" 
+data = np.load(filename)["data"]
+fft_data = (np.fft.fft(data)) 
 
 
 py.subplot(2,1,1)
-py.plot(time_array[0:2**9]*1e6,data[0:2**9])
+py.plot(time_array[0:2**10]*1e6,data[0:2**10])
 py.xlim([10,40])
 py.xlabel('time ($\mu s$)')
 py.ylabel('Voltage (V)')
-py.title('Upper Side Bound')
 
 py.subplot(2,1,2) 
 py.plot(np.fft.fftfreq(N,delt)/1e6,fft_data/1e6)
